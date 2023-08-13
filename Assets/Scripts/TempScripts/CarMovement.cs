694dee4e7d10f8f21bd5b1f858c3ca5d5a8048fb
AlexMalakov <alexander.malakov@gmail.com>
2023-08-13 12:10:45 -0400

    spring part up down
    now need to do vertical and horizontal

--- Assets/Scripts/TempScripts/CarMovement.cs
+++ Assets/Scripts/TempScripts/CarMovement.cs
@@ -5,24 +5,37 @@ using UnityEngine.InputSystem;
 
 public class CarMovement : MonoBehaviour
 {
+    [Header ("speed")]
     public float speed;
     public float maxSpeed;
     public float speedIncrease;
+    public float turnSpeed;
+
+    [Header ("gravity bounce and collisions")]
+    public float distOffGround;
+    public LayerMask ignoreMe;
+    public float springStrength;
+    public float stringDamper;
+
+    public float rotSpringStrength;
+    public float rotSpringDamper;
+
+    Quaternion uprightRotationTarget;
 
     public float maxForceBoost;
 
+    [Header ("tripping")]
     public RoadsController road;
     public float tripTime;
     public float tripCooldown;
+
     bool canTrip;
     WorldState state;
 
-    public float turnSpeed;
-
+    [Header ("hp and points")]
     public int maxHealth;
     int health;
     public int pointsToHeal;
-
     int score;
     int currentScore;
 
@@ -31,6 +44,7 @@ public class CarMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        uprightRotationTarget = transform.rotation;
         score = 0;
         currentScore = 0;
         health = maxHealth;
@@ -45,6 +59,10 @@ public class CarMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if(Input.GetKeyDown(KeyCode.Q)) {
+            transform.position = new Vector3(transform.position.x,transform.position.y + 2,transform.position.z);
+        }
         if(Input.GetKeyDown(KeyCode.Space) && canTrip) {
             canTrip = false;
             state = WorldState.FANTASY;
@@ -68,10 +86,13 @@ public class CarMovement : MonoBehaviour
     void FixedUpdate() {
 
         rigid.MoveRotation(rigid.rotation * Quaternion.Euler(0f, Input.GetAxis("Horizontal") * turnSpeed, 0f));
-        
-        rigid.velocity = speed * transform.right.normalized;
+        Vector3 xzSpeed = speed * transform.right.normalized;
+        rigid.velocity = new Vector3(xzSpeed.x, rigid.velocity.y, xzSpeed.z);
+
+        rideGround();
     }
 
+  
     public void boostSpeed(WorldState state) {
         if(state == WorldState.FANTASY) {
             speed *= speedIncrease;
@@ -99,5 +120,38 @@ public class CarMovement : MonoBehaviour
     //how hard I hit the
     public float getSpeedScalar() {
         return 1+(speed*maxForceBoost/maxSpeed);
+    }
+
+    void rideGround() {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, -transform.up, out hit, 200f, ~ignoreMe)) {
+
+            Debug.Log("HIT: " + hit.distance + ", " + hit.collider.gameObject.name);
+            
+
+            float rayDirVel = Vector3.Dot(-transform.up, rigid.velocity);
+
+            float x = hit.distance - distOffGround;
+
+            float springForce = (x * springStrength) - (rayDirVel * stringDamper);
+            Debug.Log("SP STR: " + (x * springStrength) + ", DAMPER: " + (rayDirVel * stringDamper) + ", TOTAL: " + springForce + ", YVEL: " + rigid.velocity.y);
+
+            rigid.AddForce(-transform.up * springForce, ForceMode.Force);
+
+            // Debug.Log("RIGIDV: " + rigid.velocity);
+        } else {
+            Debug.Log("HUGE PROBLEM NO GROUND");
+        }
+    }
+
+    public void carTorque() {
+        // Quaternion goal = Utils.Math.ShortestRotation(uprightRotationTarget, transform.rotation);
+
+        // Vector3 axis;
+        // float rotationDegrees;
+
+        // goal.ToAngleAxis(out rotationDegrees, out axis);
+
+        // rigid.AddTorque((axis * (rotationDegrees * Mathf.Deg2Rad * rotSpringStrength)) - (rigid.angularVelocity * rotSpringDamper));
     }
 }
